692a16bbe79037a580f9a5d0acb9ea497e99b706
Sulaiman Sanaullah <ssanaul20@gmail.com>
2018-04-22 02:11:12 -0500

    nvm idk

--- p5.py
+++ p5.py
@@ -15,7 +15,7 @@ def smallest_common_multiple():
     divisors = range(11, 21)
     multiple = 2520
     iter = 0
-    for i in range(1, 20):
+    for i in range(1, 21):
         iter += i
     while(scp == 0):
         for i in divisors:
