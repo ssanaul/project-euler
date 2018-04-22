f199a957d3b54e1ed436dd284bcb422b1ae353c8
Sulaiman Sanaullah <ssanaul20@gmail.com>
2018-04-22 01:43:14 -0500

    create p5

---
+++ p5.py
@@ -0,0 +1,35 @@
+"""
+Smallest multiple
+Problem 5
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+import time
+
+print("Computing smallest multiple common to n: 1-20...")
+start = time.time()
+
+def smallest_common_multiple():
+    scp = 0
+    divisors = range(11, 21)
+    multiple = 2520
+    iter = 0
+    for i in range(1, 21):
+        iter += i
+    while(scp == 0):
+        for i in divisors:
+            remainder = False
+            if multiple%i != 0:
+                remainder = True
+                break
+        if remainder == False:
+            scp = multiple
+        multiple += iter
+    return scp
+
+print(smallest_common_multiple())
+#232792560
+
+end = time.time()
+print("Computed in "+str(end-start)+" seconds.")
